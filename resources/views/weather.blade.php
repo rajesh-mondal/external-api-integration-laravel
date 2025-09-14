b80f4722d40b800d9d4d118381e1c046873e75ec
Rajesh Mondal <rajesh01749@gmail.com>
2025-09-14 22:38:53 +0600

    display fetched weather data on frontend

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -10,6 +10,30 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
 </head>
 <body>
-
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Location</th>
+                    <th>Country</th>
+                    <th>Tempreture</th>
+                    <th>Wind Speed</th>
+                    <th>Humidity</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><img src="{{ $weather['current']['condition']['icon'] }}" alt="icon"></td>
+                    <td>{{ $weather['location']['name'] }}</td>
+                    <td>{{ $weather['location']['country'] }}</td>
+                    <td>{{ $weather['current']['temp_c'] }}°C </td>
+                    <td>{{ $weather['current']['wind_kph'] }} km/h</td>
+                    <td>{{ $weather['current']['humidity'] }}% </td>
+                    <td>{{ $weather['current']['condition']['text'] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
